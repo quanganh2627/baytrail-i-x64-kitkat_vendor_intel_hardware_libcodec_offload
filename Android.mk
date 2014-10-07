3e5a1f5dcab9f2c3f63738a3a960e7da28f4fcc6
Clement Calmels <clement.calmels@intel.com>
2014-10-07 11:48:30 +0200

    LOCAL_MODULE_PATH not supported in multiarch builds
    Change LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH as expected by
    build/core/shared_library.mk:
    
    "LOCAL_MODULE_PATH for shared libraries is unsupported in multiarch
    builds, use LOCAL_MODULE_RELATIVE_PATH instead"
    
    Change-Id: I566374c223b235da5110e04286d0eb9716d5eab2
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-8086
    Signed-off-by: Clement Calmels <clement.calmels@intel.com>
    Signed-off-by: Robert Jarzmik <robert.jarzmik@intel.com>
    Reviewed-on: https://android.intel.com:443/291371

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.codec_offload.$(TARGET_DEVICE)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE__RELATIVE_PATH := hw
 #LOCAL_CFLAGS := -std=c99
 LOCAL_SRC_FILES := codec_offload_hal.cpp
 LOCAL_SHARED_LIBRARIES := liblog libcutils \
